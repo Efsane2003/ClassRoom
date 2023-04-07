d03c10c6ecd27ff9fe7825582d10d5bbeca737f9
Abbasova Afsana <126772571+Efsane2003@users.noreply.github.com>
2023-04-07 06:18:55 -0700

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,37 @@
+// for(i=0;i<11;i++){
+//     console.log(`${i}x${i}  ${i*i}`)
+// }
+// for(i=0;i<=10;i++){
+//   console.log(`${i}x${i} =  ${i**2} ${i**3}`)
+// }
+
+
+
+
+
+let myName="Afsana";
+for(let i=0; i<myName.length; i++){
+    console.log(myName[i])
+}
+
+
+
+for(let i=0;i<9; i++){
+
+    if(i%2==0)
+    console.log(i)
+}
+
+for(let i=0; i<=7; i++){
+    if(i==0){
+        continue}
+    
+    else if( i%2==0)
+    console.log(`even:${i}`)
+    else{
+        console.log(`odd :${i}`)
+    }
+
+}
+
+
